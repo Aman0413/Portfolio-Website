7b9f992d1c48a61ea5814f7a3e74a3387f5065b8
Aman Verma <aman@Amans-MacBook-Air.local>
2023-03-28 20:02:11 +0530

    base url

--- src/axiosClient/axiosClient.js
+++ src/axiosClient/axiosClient.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+let BASE_URL = "http://localhost:4000";
+if (process.env.NODE_ENV) {
+  BASE_URL = process.env.BASE_URL;
+}
 export default axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: BASE_URL,
 });
